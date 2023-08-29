554a9a3108877520a2ac322471f2e661bca65fb2
alvinlin0824 <alvin.lin@abbott.com>
2023-08-29 10:12:40 -0700

    Commit From VS

--- HW4.cpp
+++ HW4.cpp
@@ -3,75 +3,45 @@
 // Do not check the format of user input
 // When the input answer is the same as the title, the program ends
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 int main() {
-    int answer[4] = {1, 2, 3, 4};
-    int guess[4];
-    int A = 0, B = 0;
-    while (true) {
-        cout << "Enter four non-repeating numbers (0~9) as the computer's question: ";
-        for (int i = 0; i < 4; ++i) {
-            cin >> answer[i];
-        }
-        bool valid = true;
-        for (int i = 0; i < 4; ++i) {
-            for (int j = i + 1; j < 4; ++j) {
-                if (answer[i] == answer[j]) {
-                    valid = false;
-                    break;
-                }
-            }
-            if (!valid) {
-                break;
-            }
-        }
-        if (!valid) {
-            cout << "Enter the wrong value range, the program will end directly after the error is displayed" << endl;
-            break;
-        }
-        while (true) {
-            cout << "Enter four non-repeating numbers (0~9) as your guessed answer: ";
-            for (int i = 0; i < 4; ++i) {
-                cin >> guess[i];
-            }
-            valid = true;
-            for (int i = 0; i < 4; ++i) {
-                for (int j = i + 1; j < 4; ++j) {
-                    if (guess[i] == guess[j]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (!valid) {
-                    break;
-                }
-            }
-            if (!valid) {
-                cout << "Enter the wrong value range, please re-enter" << endl;
-                continue;
-            }
-            A = B = 0;
-            for (int i = 0; i < 4; ++i) {
-                if (guess[i] == answer[i]) {
-                    ++A;
-                } else {
-                    for (int j = 0; j < 4; ++j) {
-                        if (guess[i] == answer[j]) {
-                            ++B;
-                            break;
-                        }
-                    }
-                }
-            }
-            cout << A << "A" << B << "B" << endl;
-            if (A == 4) {
-                cout << "Congratulations! You win!" << endl;
-                return 0;
-            }
-        }
-    }
+    
+    cout << "Please enter anser:" << endl;
+    int num;
+    cin >> num;
 
-    return -1;
+    int guess, bulls, cows;
+    // cout << "Welcome to the 1A2B game! Try to guess the 4-digit number I'm thinking of." << endl;
+    do {
+        bulls = cows = 0;
+        cout << "Please enter guess:" << endl;
+        cin >> guess;
+        int num_copy = num, guess_copy = guess;
+        for (int i = 0; i < 4; i++) {
+            // Remainder
+            int num_digit = num_copy % 10;
+            int guess_digit = guess_copy % 10;
+            // Position and Number are matched
+            if (num_digit == guess_digit) {
+                bulls++;
+            } else if (num % 10 == guess_digit || num % 100 == guess_digit || num % 1000 == guess_digit) {
+                cows++;
+            }
+            num_copy /= 10;
+            guess_copy /= 10;
+        }
+        if (bulls == 4) {
+            cout << bulls << "A" << cows << "B" << endl;
+            cout << "You Win!" << endl;
+            break;
+        } else {
+            cout << bulls << "A" << cows << "B" << endl;
+        }
+    } while (true);
+    return 0;
 }
+
