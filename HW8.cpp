cc857642acc2b839955006230ec86482ea6c3db9
alvinlin0824 <alvin.lin@abbott.com>
2023-08-31 21:53:55 -0700

    Commit From HVS

---
+++ HW8.cpp
@@ -0,0 +1,14 @@
+// Design a class of eCash to simulate the behavior of stored-value cards
+// Private members:
+// Attribute : Money currently available amount
+// Public members:
+// Method: eCash () constructor (initialize Money to 0 yuan)
+// Method: void store (int m) store value (store m in Money)
+// Method: void pay (int m) consumption (consume Money for m yuan)
+// (Reminder: It is necessary to judge whether the current eCash has sufficient amount for consumption)
+// Method: void display() Display the current balance (output Money to the screen)
+// Main program
+// Enter 's': stored value
+// Type 'p': spend
+// Enter 'd': check balance
+// Type 'q': exit the program
